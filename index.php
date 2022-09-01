8461440fbdfe35c156e35daf4e0db5639e33e138
cyrille marc <cyrillemarc1@gmail.com>
2022-09-01 11:54:16 +0200

    remove last commit

--- index.php
+++ index.php
@@ -99,7 +99,6 @@
         </div>
     </div>
     <h3 id="TitleMaTournee">Mes dates de tournée.
-        <h6 id="paysage">Passez en mode paysage pour les consulter></h6>
         <div id="Tournee">
             <table>
                 <thead>
@@ -454,14 +453,11 @@
         font-weight: 900;
     }
 
-    @media screen and (min-width: 480px) and (max-width: 720px) {
+    @media screen and (min-width: 200px) and (max-width: 750px) {
         #Header {
             display: flex;
             flex-direction: column;
             padding: 30px;
-        }
-        .clignote{
-            font-size: 15px;
         }
         #Title{
             font-size: 2em;
@@ -473,12 +469,4 @@
             display: none;
         }
     }
-    @media screen and (min-width: 720 px) and (max-width: 1200px) {
-        #paysage {
-            display: none;     
-        }
-        
-    }
-    @media screen and (min-width: 721px) and (max-width: 900px) {}
-    @media screen and (min-width: 901px) and (max-width: 1024) {}
 </style>
